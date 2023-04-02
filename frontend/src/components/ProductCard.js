bf7941f1079e222afb5d3f0f9f8e0464d92c14d6
Sotirios Karnaros <sotirios.karnaros@citrix.com>
2023-04-02 13:32:36 +0300

    fixes hierarchy of obects in ProductCard

--- frontend/src/components/ProductCard.js
+++ frontend/src/components/ProductCard.js
@@ -8,35 +8,41 @@ import Carousel from 'react-material-ui-carousel'
 const ProductCard = (props) => {
 
   return (
-      <Card sx={{ maxWidth: 250 }}>
-        <CardActionArea>
-          <Carousel autoPlay={false}>
-            {
-                props.data.image_urls.map( (image_url, index) =>
-                    <CardMedia
-                    key={index}
-                    component="img"
-                    image={image_url}
-                    alt={image_url}
-                    />
-                )
-            }
-          </Carousel>
-          <CardContent>
-            <Typography gutterBottom variant="h7" sx={{ marginBottom: '1.35em', display: "block", maxHeight: '50px', textOverflow: 'ellipsis'
-            , overflow: "hidden"}} >
-              {props.data.product_title}
-            </Typography>
+    <Card sx={{ maxWidth: 250 }}>
+      <Carousel autoPlay={false}>
+        {
+          props.data.image_urls.map((image_url, index) =>
+            <CardMedia
+              key={index}
+              component="img"
+              image={image_url}
+              alt={image_url}
+            />
+          )
+        }
+      </Carousel>
+      <CardActionArea>
+        <CardContent>
+          <Typography gutterBottom variant="h7"
+            sx={{
+              marginBottom: '1.35em',
+              display: "block",
+              maxHeight: '50px',
+              textOverflow: 'ellipsis',
+              overflow: "hidden"
+            }} >
+            {props.data.product_title}
+          </Typography>
 
-              <Typography gutterBottom variant="subtitle2">
-                {"Price: " + props.data.price + " " + props.data.currency_code}
-              </Typography>
-              <Typography gutterBottom variant="subtitle2" align="right">
-                {"Stock: " + props.data.stock}
-              </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
+          <Typography gutterBottom variant="subtitle2">
+            {"Price: " + props.data.price + " " + props.data.currency_code}
+          </Typography>
+          <Typography gutterBottom variant="subtitle2" align="right">
+            {"Stock: " + props.data.stock}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
   );
 }
 
